90b83540eb548b270787643e8a00d61e2344061c
ltalavera19 <ltalavera@hawk.iit.edu>
2022-04-25 19:22:40 -0500

    added

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function initMap() {
+    const chicago = { lat: 41.8781, lng: -87.6298 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 6,
+        center: chicago,
+        mapTypeId: "terrain",
+    });
+
+    const marker = new google.maps.Marker({
+        position: chicago,
+        map: map,
+    });
+
+    chicagoPath.setMap(map);
+
+} 
